798210b2da39d7a17bc66da96bbcc27616914d4a
vansh Sharma <vanshsharma7832@gmail.com>
2025-12-09 10:38:12 +0530

    Added not to shut down

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 const express=require("express");
+const axios =require("axios")
+const url="https://vashu-sportfolio.onrender.com/";
+
+const interval_time=30000;
+
+setInterval(()=>{
+    axios.get(url).then((data)=>{
+        console.log("website loaded");
+    })
+    .catch((err)=>{
+        console.log("error occured")
+    })
+
+
+},interval_time);
+
 
 
 const app=require("./src/app")
